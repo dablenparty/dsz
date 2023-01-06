400c8ba862b10d8c7dd54895149ccbb9e37c4aea
dablenparty <hdavenport8902@gmail.com>
2023-01-06 16:49:03 -0500

    Implemented proof of concept tree building

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::{
 };
 
 use clap::Parser;
+use walkdir::WalkDir;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -13,6 +14,10 @@ struct Args {
     /// The directory to calculate the size of. If not specified, the current directory is used.
     #[arg(default_value = ".")]
     dir: PathBuf,
+    #[arg(short, long)]
+    list: bool,
+    #[arg(short, long, default_value = "1")]
+    depth: usize,
 }
 
 fn recursive_dir_size(dir: &Path) -> io::Result<u64> {
@@ -54,9 +59,68 @@ fn size_in_bytes_pretty_string(size: u64) -> String {
     format!("{size:.2} {size_str}")
 }
 
+fn tree_string(dir: &Path, max_depth: usize) -> String {
+    const VERTICAL: &str = "│   ";
+    const BRANCH: &str = "├── ";
+    const LAST_BRANCH: &str = "└── ";
+
+    let initial_depth = dir.components().count();
+
+    let strings: Vec<String> = WalkDir::new(dir)
+        .max_depth(max_depth)
+        .sort_by_file_name()
+        .into_iter()
+        .map(|f| {
+            let f = f.unwrap();
+            let path = f.path();
+            let depth = path.components().count() - initial_depth;
+            let is_last = (path.is_file() || depth == max_depth)
+                && WalkDir::new(path.parent().unwrap())
+                    .sort_by_file_name()
+                    .max_depth(1)
+                    .into_iter()
+                    .last()
+                    .map(|f| f.unwrap())
+                    .map(|f| f.path().to_path_buf())
+                    .map_or(false, |p| p == path);
+            let prefix = if depth == 0 {
+                String::new()
+            } else {
+                let mut prefix = String::new();
+                for _ in 0..depth - 1 {
+                    prefix.push_str(VERTICAL);
+                }
+                prefix.push_str(if is_last { LAST_BRANCH } else { BRANCH });
+                prefix
+            };
+            let suffix = if path.is_file() || depth == max_depth {
+                let mut s = String::new();
+                let size = if path.is_dir() {
+                    s.push('/');
+                    recursive_dir_size(path).unwrap_or(0)
+                } else {
+                    path.metadata().map_or(0, |m| m.len())
+                };
+                let size_str = size_in_bytes_pretty_string(size);
+                format!("{s}: {size_str}")
+            } else if path.is_dir() {
+                String::from("/")
+            } else {
+                String::new()
+            };
+            format!("{prefix}{}{suffix}", f.file_name().to_string_lossy())
+        })
+        .collect();
+    strings.join("\n")
+}
+
 fn main() {
     let args = Args::parse();
     let canon_dir = dunce::canonicalize(args.dir).unwrap();
+    if args.list {
+        let tree = tree_string(&canon_dir, args.depth);
+        println!("{}\n", tree);
+    }
     let size = recursive_dir_size(&canon_dir).unwrap();
     let size_str = size_in_bytes_pretty_string(size);
     println!("{}: {size_str}", canon_dir.display());
