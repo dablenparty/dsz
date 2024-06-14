b4060113a45668d4b0633b35cfa3eb5cad52edfa
dablenparty <hdavenport8902@gmail.com>
2024-06-14 16:07:18 -0400

    Implement completion generation for zsh

--- src/bin/completion.rs
+++ src/bin/completion.rs
@@ -1,5 +1,19 @@
 #![cfg(feature = "completion")]
 
-fn main() {
-    println!("Hello, world!");
+use clap::CommandFactory;
+use clap_complete::shells;
+
+fn main() -> anyhow::Result<()> {
+    const PKG_NAME: &str = env!("CARGO_PKG_NAME");
+    const PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+    // TODO: support other shells (the enum already has impl FromStr)
+
+    println!("# {PKG_NAME} {PKG_VERSION} completion script for Zsh");
+
+    let mut cmd = cli::Args::command();
+
+    clap_complete::generate(shells::Zsh, &mut cmd, PKG_NAME, &mut std::io::stdout());
+
+    Ok(())
 }
